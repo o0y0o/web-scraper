019fd4d6c66b39acfd667df923bd57aeead20509
Jason Chung <shiningjason1989@gmail.com>
2021-02-08 14:04:17 +0800

    feat: enhance http out plugin error handler (#14)
    Co-authored-by: Jason Chung <jason.chung@skylinetw.com>

--- packages/scraper-plugin-http/index.js
+++ packages/scraper-plugin-http/index.js
@@ -2,5 +2,10 @@ const execHttp = require('@0y0/exec-http')
 
 module.exports = {
   in: execHttp,
-  out: execHttp
+  out: async (...args) => {
+    const resp = await execHttp(...args)
+    if (resp instanceof Error) throw resp
+    const msg = `request to ${resp.url} failed, reason: ${resp.status}`
+    if (resp.status !== 200) throw new Error(msg)
+  }
 }
