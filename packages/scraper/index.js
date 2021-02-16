ec14636ca8068c73fa9d79d960345930bfa99bbd
jason.chung <shiningjason1989@gmail.com>
2021-02-16 14:16:36 +0800

    update dependencies

--- packages/scraper/index.js
+++ packages/scraper/index.js
@@ -1,11 +1,10 @@
-const { job } = require('cron')
 const exec = require('./lib/exec')
 
 module.exports = function runScraper(task, cb) {
   if (Array.isArray(task)) {
     task.forEach(subTask => runScraper(subTask, cb))
   } else if (task.cronTime != null) {
-    job({
+    require('cron').job({
       start: true,
       utcOffset: 0,
       cronTime: task.cronTime,
